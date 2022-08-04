044534fbf62e91ead631ce872d2201719c8d69dd
Fabio Costa <fabio.costta@yahoo.com.br>
2022-08-04 03:08:26 -0300

    auth

--- tax_br/resources/views/template/index.blade.php
+++ tax_br/resources/views/template/index.blade.php
@@ -16,18 +16,18 @@
     <title>@yield('title')</title>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid" >
-    <a class="h1 navbar-brand fw-bold " href="#" style="color: #db4648">...:: CRUD TAX-BR ::...</a>
+  <div class="container-fluid" style="background-color: #db4648;">
+    <a class="navbar-brand fw-bold " href="#" style="color: #fefefe">...:: CRUD TAX-BR ::...</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active fw-bold " aria-current="page" href="#">Home</a>
+          <a class="nav-link active fw-bold " aria-current="page" href="#"></a>
         </li>
         <li class="nav-item dropdown fw-bold">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #fefefe">
             Usuários
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -36,7 +36,7 @@
           </ul>
         </li>
         <li class="nav-item dropdown fw-bold">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #fefefe">
             Impostos
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -46,6 +46,38 @@
         </li>
       </ul>
     </div>
+                        <div class="col-2">
+                            <ul class="navbar-nav mr-auto">
+                                @if(Auth::user())
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="#">{{ Auth::user()->name }}</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <x-responsive-nav-link class="nav-link text-white" :href="route('logout')"
+                                                    onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                                {{ __('Sair') }}
+                                            </x-responsive-nav-link>
+                                        </form>
+                                    </li>                        
+                                @else
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="{{ route('login') }}">Entrar</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="{{ route('register') }}">Registrar-se</a>
+                                    </li>   
+                                @endif
+                            </ul>
+                        </div>
+
+
+
+
+
+
   </div>
 </nav>
 
